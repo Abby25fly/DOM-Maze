72c5dc14b2e136cbcf009df3292be8ee5599b1aa
Abigail <abi25.fly@gmail.com>
2017-08-08 10:06:58 -0500

    con flechas-funciona

--- Js/maze.js
+++ Js/maze.js
@@ -1,6 +1,6 @@
-var mostrar = document.getElementById("mostrarLaberinto");
 var izquierda = document.getElementById("Izquierda");
-var avanzar = document.getElementById("Avanzar");
+var arriba = document.getElementById("Arriba");
+var abajo = document.getElementById("Abajo");
 var derecha = document.getElementById("Derecha");
 
 var mapa=[
@@ -15,8 +15,11 @@ var mapa=[
 "*_**_**_*****_**_*",
 "*o*__*________**W*",
 "******************"];
+var positionI;
+var positionJ;
 
 function dibujarLaberinto(){
+  var mostrar = document.getElementById("mostrarLaberinto");
   var tabla=document.createElement("table");
     for(var i = 0; i < mapa.length; i++){
       var fila = document.createElement("tr");
@@ -29,11 +32,16 @@ function dibujarLaberinto(){
           celda.setAttribute("class","colorLaberinto");
         }
         else if(mapa[i][j]=="o"){
+          positionJ=j;
+          positionI=i;
           celda.setAttribute("class","colorStart");
         }
         else if(mapa[i][j]=="W"){
           celda.setAttribute("class","colorEnd");
         }
+        celda.setAttribute("id", i + "," + j)
+	      var indices = document.createElement("div");
+        celda.appendChild(indices);
         fila.appendChild(celda);
       }
       tabla.appendChild(fila);
@@ -42,3 +50,53 @@ function dibujarLaberinto(){
 }
 
 dibujarLaberinto();
+
+// Flechas
+Arriba.onclick = function () {
+    var img = document.createElement("IMG");
+    img.src = "Css/up.png";
+	    if (mapa[(positionI - 1)][positionJ] != "*") {
+	        var stepInit = positionI + "," + positionJ;
+	        var nextStep = (positionI - 1) + "," + positionJ;
+	        document.getElementById(stepInit).innerHTML = "";
+	        document.getElementById(nextStep).appendChild(img);
+	        positionI--;
+	    }
+      console.log(stepInit);
+	}
+
+Derecha.onclick = function () {
+    var imgRight = document.createElement("IMG");
+    imgRight.src = "Css/right.png";
+    if (mapa[positionI][positionJ + 1] != "*") {
+        var stepInit = positionI + "," + positionJ;
+        var nextStep = positionI + "," + (positionJ + 1);
+        document.getElementById(nextStep).appendChild(imgRight);
+        document.getElementById(stepInit).innerHTML = "";
+        positionJ++;
+    }
+}
+
+Izquierda.onclick = function () {
+    var imgLeft = document.createElement("IMG");
+    imgLeft.src = "Css/left.png";
+	    if (mapa[positionI][positionJ - 1] != "*") {
+	        var stepInit = positionI + "," + positionJ;
+	        var nextStep = positionI + "," + (positionJ - 1);
+	        document.getElementById(nextStep).appendChild(imgLeft);
+	        document.getElementById(stepInit).innerHTML = "";
+	        positionJ--;
+	    }
+	}
+
+  Abajo.onclick = function () {
+    var imgDown = document.createElement("IMG");
+    imgDown.src = "Css/down.png";
+	    if (mapa[(positionI + 1)][positionJ] != "*") {
+	        var stepInit = positionI + "," + positionJ;
+	        var nextStep = (positionI + 1) + "," + positionJ;
+	        document.getElementById(stepInit).innerHTML = "";
+	        document.getElementById(nextStep).appendChild(imgDown);
+	        positionI++;
+	    }
+	}
